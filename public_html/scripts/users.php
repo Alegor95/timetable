ef6c80e92b4ae2caea42eea6c578ac1653d19ab9
Aleksandr Gorovoi <alexander.gorovoy@vistar.su>
2017-04-11 14:55:15 +0300

    Исправил сообщение на локализованный

--- public_html/scripts/users.php
+++ public_html/scripts/users.php
@@ -29,7 +29,7 @@
 						}
 					} else {
 						header('HTTP/1.1 404 Not found');
-						echo json_encode(array("error"=>"User ".$email."/".$pass." not found"));
+						echo json_encode(array("error"=>"Пользователь с такими данными не найден"));
 					}
 				} break;
 				case 'register':{
