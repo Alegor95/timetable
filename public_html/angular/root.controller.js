38254e93e0a1b8728d81d686dbfff49376c67505
Aleksandr Gorovoi <alexander.gorovoy@vistar.su>
2017-04-11 15:10:34 +0300

    Редирект для незалогиненых пользователей

--- public_html/angular/root.controller.js
+++ public_html/angular/root.controller.js
@@ -1,5 +1,5 @@
 (function(app) {
-  app.controller('rootController', function($scope, usersService) {
+  app.controller('rootController', function($scope, usersService, $location) {
       usersService.getLogged(function(user){
         $scope.authorized = user;
       });
@@ -7,14 +7,22 @@
       $scope.logout = function() {
         $scope.authorized = null;
         usersService.logout();
-      }
+      };
 
       $scope.login = function(login, password, callback) {
         usersService.login(login, password, function(user) {
           $scope.authorized = user;
           callback(user);
         });
-      }
+      };
+
+      $scope.$on("$locationChangeStart", function(event, next, current) {
+        if (!$scope.authorized
+            && next.indexOf('login') < 0
+            && next.indexOf('register') < 0) {
+          $location.url('login');
+        }
+      });
     }
   );
 })(window.app);
